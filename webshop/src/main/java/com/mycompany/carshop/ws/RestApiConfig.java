21d16f0c996ac0405b6e4105a0d185ff145695af
cfhpanteracfh <stefanjovanic@yahoo.com>
2017-11-22 14:54:40 +0100

    CarShop: Origin issue solved

--- webshop/src/main/java/com/mycompany/carshop/ws/RestApiConfig.java
+++ webshop/src/main/java/com/mycompany/carshop/ws/RestApiConfig.java
@@ -2,9 +2,12 @@ package com.mycompany.carshop.ws;
 
 import org.glassfish.jersey.server.ResourceConfig;
 
+import com.mycompany.carshop.port.impl.CORSResponseFilter;
+
 public class RestApiConfig extends ResourceConfig {
 
     public RestApiConfig() {
         packages("com.mycompany.carshop.port.impl");
+        register(CORSResponseFilter.class);
     }
 }
