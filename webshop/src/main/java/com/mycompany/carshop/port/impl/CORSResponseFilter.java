86f95533186bc57ca369bbaf8523b11a60be5d18
nakotomagano <veljko7@yahoo.com>
2017-12-07 15:45:51 +0100

    added missing java file

---
+++ webshop/src/main/java/com/mycompany/carshop/port/impl/CORSResponseFilter.java
@@ -0,0 +1,26 @@
+package com.mycompany.carshop.port.impl;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.MultivaluedMap;
+
+public class CORSResponseFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        MultivaluedMap<String, Object> headers = responseContext.getHeaders();
+
+        headers.add("Access-Control-Allow-Origin", "*");
+        //headers.add("Access-Control-Allow-Origin", "http://localhost"); //allows CORS requests only coming from localhost
+        headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+        headers.add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Codingpedia");
+
+    }
+
+
+
+
+}
